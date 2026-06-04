23c5fcb195c671505a6818eadbb0f1d5a87b3408
suratron <darfypoo@gmail.com>
2026-06-04 04:06:35 -0400

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -2,7 +2,7 @@
   "name": "Phantom Wallet",
   "short_name": "Phantom",
   "description": "Phantom crypto wallet portfolio tracker",
-  "start_url": "./phantom.html",
+  "start_url": "./index.html",
   "display": "standalone",
   "background_color": "#000000",
   "theme_color": "#000000",
