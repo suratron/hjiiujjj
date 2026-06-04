35b5176ccdccd8c37933c22e00e1ac81c8685826
suratron <darfypoo@gmail.com>
2026-06-04 03:55:57 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,38 @@
-const CACHE_NAME = 'phantom-cache-v1';
-const urlsToCache = [
-  '/',
-  '/phantom.html'
+const CACHE_NAME = 'phantom-v1';
+const ASSETS = [
+  './phantom.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap',
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
+      return cache.addAll(ASSETS.filter(url => !url.startsWith('http')));
     })
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request).catch(() => {
+        if (event.request.destination === 'document') {
+          return caches.match('./phantom.html');
+        }
+      });
     })
   );
 });
